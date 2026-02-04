43f7f4a0cb7fdcd8ee7fe62230c39b73be9e64c2
dimbo4ka <dimbo4kaIT@gmail.com>
2026-02-04 18:49:45 +0300

    solve 'fetchall'

--- fetchall/main.go
+++ fetchall/main.go
@@ -2,6 +2,38 @@
 
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
 
+func main() {
+	args := os.Args[1:]
+	now := time.Now()
+
+	var wg sync.WaitGroup
+
+	for _, url := range args {
+		wg.Add(1)
+
+		go func(url string) {
+			defer wg.Done()
+
+			resp, err := http.Get(url)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer resp.Body.Close()
+
+			bytes, _ := io.Copy(io.Discard, resp.Body)
+			log.Printf("%6.3fs %7d %s\n", time.Since(now).Seconds(), bytes, url)
+		}(url)
+	}
+	wg.Wait()
+	log.Printf("Total time: %6.3fs\n", time.Since(now).Seconds())
 }
